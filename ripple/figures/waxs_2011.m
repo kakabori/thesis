f1a49ebb68538b266dac45dd580b20297db4b5d6
Kiyo <kakabori@andrew.cmu.edu>
2014-07-15 20:24:28 -0400

    rewrote nGIWAXS result section

--- ripple/figures/waxs_2011.m
+++ ripple/figures/waxs_2011.m
@@ -17,82 +17,112 @@ sDist = 169.8;
 beamX = 32;
 beamZ = 110; % same as X_cen = 1024 - 110 + 1 = 915;
 
-a = slurp('ripple_060_cz.tif', 'c');
-a = flipud(a);
-fig1 = figure;
-imagesc(a, [0 1000])
-colormap(gray)
-set(gca, 'TickDir', 'out', 'FontSize', 16, 'FontName', 'Times New Roman')
-axis image
-axis xy
-saveTightFigure(fig1, 'ripple_060.pdf')
-
-b = slurp('ripple_061_cz.tif', 'c');
-b = flipud(b);
-fig2 = figure;
-imagesc(b, [0 1000])
-colormap(gray)
-set(gca, 'TickDir', 'out', 'FontSize', 16, 'FontName', 'Times New Roman')
-axis image
-axis xy
-saveTightFigure(fig2, 'ripple_061.pdf')
-
-tmp = a - b;
-fig3 = figure;
-imagesc(tmp, [0 300])
-colormap(gray)
-set(gca, 'TickDir', 'out', 'FontSize', 16, 'FontName', 'Times New Roman')
-axis image
-axis xy
-saveTightFigure(fig3, 'ripple_060_ccd.pdf')
-
-% ripple phase data
-a = slurp('ripple_060_cz.tif', 'c');
-b = slurp('ripple_061_cz.tif', 'c');
-tmp = a - b;
-tmp = rotateAround(tmp, 915, 32, -0.97, 'bicubic');
-waxs1 = transform_ccd2q(tmp, [0 2.2], [0 2], 0.0022, 0.0022, 0.2);
-waxs1.Int = int64(waxs1.Int);
-fig4 = figure;
-qshow(waxs1, [0 300]);
-saveTightFigure(fig4, 'ripple_060_q.pdf');
-%axis([1 1.8 0 1])
-%saveas(fig4, 'ripple_060.pdf');
-%saveTightFigure(fig4, 'ripple_060.pdf')
+% % ripple phase data
+% a = slurp('ripple_060_cz.tif', 'c');
+% a = flipud(a);
+% fig1 = figure;
+% imagesc(a, [0 1000])
+% colormap(gray)
+% set(gca, 'TickDir', 'out', 'FontSize', 16, 'FontName', 'Times New Roman')
+% axis image
+% axis xy
+% saveTightFigure(fig1, 'ripple_060.pdf')
+% 
+% b = slurp('ripple_061_cz.tif', 'c');
+% b = flipud(b);
+% fig2 = figure;
+% imagesc(b, [0 1000])
+% colormap(gray)
+% set(gca, 'TickDir', 'out', 'FontSize', 16, 'FontName', 'Times New Roman')
+% axis image
+% axis xy
+% saveTightFigure(fig2, 'ripple_061.pdf')
+% 
+% tmp = a - b;
+% fig3 = figure;
+% imagesc(tmp, [0 300])
+% colormap(gray)
+% set(gca, 'TickDir', 'out', 'FontSize', 16, 'FontName', 'Times New Roman')
+% axis image
+% axis xy
+% saveTightFigure(fig3, 'ripple_060_ccd.pdf')
+% 
+% a = slurp('ripple_060_cz.tif', 'c');
+% b = slurp('ripple_061_cz.tif', 'c');
+% tmp = a - b;
+% tmp = rotateAround(tmp, 915, 32, -0.97, 'bicubic');
+% waxs1 = transform_ccd2q(tmp, [0 2.2], [0 2], 0.0022, 0.0022, 0.2);
+% waxs1.Int = int64(waxs1.Int);
+% fig4 = figure;
+% qshow(waxs1, [0 300]);
+% saveTightFigure(fig4, 'ripple_060_q.pdf');
+% %axis([1 1.8 0 1])
+% %saveas(fig4, 'ripple_060.pdf');
+% %saveTightFigure(fig4, 'ripple_060.pdf')
 
 % fluid phase data
-a = slurp('fluid_036_cz.tif','c');
-b = slurp('fluid_039_cz.tif','c');
+a = slurp('fluid_036_cz.tif', 'c');
+a = flipud(a);
+figure
+imagesc(a, [0 3000])
+colormap(gray)
+set(gca, 'TickDir', 'out', 'FontSize', 16, 'FontName', 'Times New Roman')
+xlabel('p_x', 'interpreter', 'tex');
+ylabel('p_z', 'interpreter', 'tex');
+axis image
+axis xy
+saveTightFigure(gcf, 'fluid_036.pdf')
+
+b = slurp('fluid_039_cz.tif', 'c');
+b = flipud(b);
+figure
+imagesc(b, [0 3000])
+colormap(gray)
+set(gca, 'TickDir', 'out', 'FontSize', 16, 'FontName', 'Times New Roman')
+xlabel('p_x', 'interpreter', 'tex');
+ylabel('p_z', 'interpreter', 'tex');
+axis image
+axis xy
+saveTightFigure(gcf, 'fluid_039.pdf')
+
 tmp = a - b;
-%tmp = tmp * 0.12;
+figure
+imagesc(tmp, [0 1000])
+colormap(gray)
+set(gca, 'TickDir', 'out', 'FontSize', 16, 'FontName', 'Times New Roman')
+xlabel('p_x', 'interpreter', 'tex');
+ylabel('p_z', 'interpreter', 'tex');
+axis image
+axis xy
+saveTightFigure(gcf, 'fluid_036_ccd.pdf')
+
+tmp = flipud(tmp);
 tmp = rotateAround(tmp, 915, 32, -0.97, 'bicubic');
 waxs2 = transform_ccd2q(tmp, [0 2.2], [0 2], 0.0022, 0.0022, 0.5);
 waxs2.Int = int64(waxs2.Int);
-fig5 = figure;
-qshow(waxs2, [0 1200]);
-%axis([1 1.8 0 1])
-%saveas(fig5, 'fluid_036.pdf');
-saveTightFigure(fig5, 'fluid_036.pdf')
+figure
+qshow(waxs2, [0 1000]);
+saveTightFigure(gcf, 'fluid_036_q.pdf')
 
-k = 0;
-for i = 0.09:0.1:0.6
-  figure 
-  [qr, Int] = qrplot_q(waxs1, [i i+0.02]);
-  avg = i + 0.01;
-  filename = strcat('qr_ripple_', num2str(k), '.dat');
-  dlmwrite(filename, [qr Int])
-  k = k + 1;
-end
-
-k = 0;
-for i = 0.09:0.1:0.6
-  figure 
-  [qr, Int] = qrplot_q(waxs2, [i i+0.02]);
-  avg = i + 0.01;
-  filename = strcat('qr_fluid_', num2str(k), '.dat');
-  dlmwrite(filename, [qr Int])
-  k = k + 1;
-end
+% k = 0;
+% for i = 0.09:0.1:0.6
+%   figure 
+%   [qr, Int] = qrplot_q(waxs1, [i i+0.02]);
+%   avg = i + 0.01;
+%   filename = strcat('qr_ripple_', num2str(k), '.dat');
+%   dlmwrite(filename, [qr Int])
+%   k = k + 1;
+% end
+% 
+% k = 0;
+% for i = 0.09:0.1:0.6
+%   figure 
+%   [qr, Int] = qrplot_q(waxs2, [i i+0.02]);
+%   avg = i + 0.01;
+%   filename = strcat('qr_fluid_', num2str(k), '.dat');
+%   dlmwrite(filename, [qr Int])
+%   k = k + 1;
+% end
 
 
 % k = 0;
